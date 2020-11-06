5a61c5472bc367b26b65bd3b664cb4f5156c25ec
Frank Carr <gundamtrooperx@gmail.com>
2020-11-05 22:38:54 -0700

    updated

--- Develop/routes/api.js
+++ Develop/routes/api.js
@@ -36,4 +36,8 @@ router.get("/", function(req, res){
   res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
+router.get("*", function(req, res){
+  res.sendFile(path.join(__dirname, "../public/index.html"))
+})
+
 module.exports = router;
