93047eb5dd851f833ee46562f5df80044a9a00e8
Frank Carr <gundamtrooperx@gmail.com>
2020-11-05 21:57:56 -0700

    updated

--- server.js
+++ server.js
@@ -15,10 +15,6 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/budget", {
-  useNewUrlParser: true,
-  useFindAndModify: false
-});
 
 // routes
 app.use(require("./Develop/routes/api.js"));
