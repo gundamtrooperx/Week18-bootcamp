b352c0c9d4791da02d8c63cf0f81208fe9dbfd20
Frank Carr <gundamtrooperx@gmail.com>
2020-11-13 22:29:32 -0700

    fixed graph loading error and added manifest, service worker, db.js

--- server.js
+++ server.js
@@ -15,9 +15,12 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 // routes
-app.use(require("./Develop/routes/api.js"));
+app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
